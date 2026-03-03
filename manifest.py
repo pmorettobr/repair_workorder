ddd71c2a2b2b41f7c52b6f4756ee8680ca5be68c
pmorettobr <pmorettobr@gmail.com>
2026-03-03 14:20:59 -0300

    Create manifest.py

---
+++ manifest.py
@@ -0,0 +1,22 @@
+{
+    "name": "Repair Workorder Community V2",
+    "version": "16.0.2.0",
+    "author": "Paulo Moretto",
+    "category": "Manufacturing",
+    "depends": [
+        "repair",
+        "mrp",
+        "hr"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/repair_order_views.xml",
+        "views/repair_workorder_views.xml"
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "repair_workorder_community_v2/static/src/js/repair_timer.js"
+        ]
+    },
+    "installable": True,
+}
